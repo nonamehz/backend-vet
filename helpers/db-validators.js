850ec77118042eda2ab6ee6078359b90b9327785
Jose Caballero <acg_1349@hotmail.com>
2022-11-18 03:07:11 -0500

    validaciones terminadas en usuarios

---
+++ helpers/db-validators.js
@@ -0,0 +1,27 @@
+const Usuario = require('../models/usuario');
+
+
+const emailExiste = async (correo = '') => {
+
+    const existeEmail = await Usuario.findOne({ where: { correo } });
+    if (existeEmail) {
+        throw new Error(`El correo ${correo} ya está registrado`);
+        // return res.status(400).json({
+        //     msg: 'Ese correo ya está registrado'
+        // });
+    }
+
+}
+
+const existeUsuarioPorId = async (id) => {
+    const existeUsuario = await Usuario.findByPk(id);
+    if (!existeUsuario) {
+        throw new Error(`El id no existe ${id}`);
+    }
+}
+
+
+module.exports = {
+    emailExiste,
+    existeUsuarioPorId
+}
--- routes/usuarios.routes.js
+++ routes/usuarios.routes.js
@@ -1,14 +1,41 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
+
 const { usuariosGet, usuariosGetById, usuariosPost, usuariosPut, usuariosDelete } = require('../controllers/usuarios.controllers');
+const { existeUsuarioPorId, emailExiste } = require('../helpers/db-validators');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 
 const router = Router();
 
 router.get('/usuarios', usuariosGet);
-router.get('/usuarios/:id', usuariosGetById);
-router.post('/usuarios', usuariosPost);
-router.put('/usuarios/:id', usuariosPut);
-router.delete('/usuarios/:id', usuariosDelete);
+
+router.get('/usuarios/:id', [
+    check('id').custom(existeUsuarioPorId),
+    validarCampos
+], usuariosGetById);
+
+router.post('/usuarios', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('apellido', 'El apellido es obligatorio').not().isEmpty(),
+    check('celular', 'El celular es obligatorio').not().isEmpty(),
+    check('password', 'La contraseña es obligatoria').not().isEmpty(),
+    check('password', 'La contraseña debe de tener un mínimo de 6 caracteres').isLength({ min: 6 }),
+    check('correo', 'El correo no es válido').isEmail(),
+    check('correo').custom(emailExiste),
+    validarCampos
+], usuariosPost);
+
+router.put('/usuarios/:id', [
+    check('id').custom(existeUsuarioPorId),
+    validarCampos
+], usuariosPut);
+
+
+router.delete('/usuarios/:id', [
+    check('id').custom(existeUsuarioPorId),
+    validarCampos
+], usuariosDelete);
 
 
 module.exports = router;
